65d716b14e0c5622db77fbe506e3749ba0a14c2d
nsaitohorizon-glitch <n.saito.horizon@gmail.com>
2026-04-28 15:36:22 +0900

    Add greet method and refactor main

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,6 +2,10 @@
 public class App {
 
     public static void main(String[] args) {
+        greet();
+    }
+
+    private static void greet() {
         System.out.println("Hello GitHub!");
     }
 
